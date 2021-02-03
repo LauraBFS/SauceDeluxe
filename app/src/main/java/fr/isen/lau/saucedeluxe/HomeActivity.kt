f83bf1e70d1e4dc715e4d7e25577bff0c39c5f9b
lau <titlaulau@aol.fr>
2021-02-03 19:33:00 +0100

    Ajout de la fenetre des categories

--- app/src/main/java/fr/isen/lau/saucedeluxe/HomeActivity.kt
+++ app/src/main/java/fr/isen/lau/saucedeluxe/HomeActivity.kt
@@ -2,10 +2,18 @@ package fr.isen.lau.saucedeluxe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Toast.makeText(getApplicationContext(), "Tu cliques sur l'entrée fraté", Toast.LENGTH_SHORT).show();
+
+        val text = "Tu cliques pas ici fraté!"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
     }
 }
---
+++ app/src/main/java/fr/isen/lau/saucedeluxe/categorie.kt
@@ -0,0 +1,11 @@
+package fr.isen.lau.saucedeluxe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class categorie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categorie)
+    }
+}
