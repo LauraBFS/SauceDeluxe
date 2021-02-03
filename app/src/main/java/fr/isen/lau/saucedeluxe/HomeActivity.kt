9db803cfe28409256cd984cedb6616363ff0f43b
lau <titlaulau@aol.fr>
2021-02-03 20:16:36 +0100

    Peut basculer entre le menu et la fenetre de categorie + retour (pour la page des entrées)

--- app/src/main/java/fr/isen/lau/saucedeluxe/HomeActivity.kt
+++ app/src/main/java/fr/isen/lau/saucedeluxe/HomeActivity.kt
@@ -1,19 +1,27 @@
 package fr.isen.lau.saucedeluxe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
+    //Declaration de mes boutons
+    lateinit var boutonEntrees : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        //Toast.makeText(getApplicationContext(), "Tu cliques sur l'entrée fraté", Toast.LENGTH_SHORT).show();
+            //initialisation du bouton
+            boutonEntrees = findViewById(R.id.btn_home) //button des entrees
 
-        val text = "Tu cliques pas ici fraté!"
-        val duration = Toast.LENGTH_SHORT
-        val toast = Toast.makeText(applicationContext, text, duration)
-        toast.show()
+            // creation de notre intent
+            val monIntent : Intent =  Intent(this,CategorieActivity::class.java)
+
+            //clic sur le bouton
+            boutonEntrees.setOnClickListener {
+                startActivity(monIntent)
+            }
     }
 }
